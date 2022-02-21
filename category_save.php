955664e9f751bbe881006f687c4b7c750cd7537b
Geoffray <coorevits_geoffray@hotmail.com>
2022-02-21 18:10:21 +0100

    cours17022022

--- category_save.php
+++ category_save.php
@@ -1,3 +1,23 @@
 <?php
 
-print_r($_POST);
+/* connection à la base de donné*/
+require_once ('connexion.php');
+
+/*verif de la conncetion*/
+/*echo get_class ($db);exit; */
+
+if(!isset($_POST ['name']) || empty($_POST['name'])){
+    echo 'le nom est obligatoire !';
+    exit;
+}
+
+$name=$_POST['name'];
+
+
+/*print_r($_POST); */
+
+$db ->query('insert into category (name) value  ("'.$name.'")');
+
+
+
+
--- connexion.php
+++ connexion.php
@@ -1,2 +1,13 @@
 <?php
 
+
+$user = 'eleve';
+$pass ='eleve';
+try {
+    $db = new PDO('mysql:host=localhost;port=3307;dbname=movie_db', $user, $pass);
+
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+
